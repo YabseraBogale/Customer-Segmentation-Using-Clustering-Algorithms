b5d2826b5b31f523f03a11ae4c372e91bc68e85c
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-03-25 23:12:05 +0300

    ok

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-import pyspark as spark
-from pyspark.ml.clustering import KMeans
-from pyspark.ml.evaluation import ClusteringEvaluator
-from pyspark.ml.feature import VectorAssembler
+import pandas as pd
 
-# Load the customer data into a DataFrame
-customer_data = spark.read.csv("Mall_Customers.csv", header=True, inferSchema=True)
 
-# Select the relevant features for clustering
-feature_cols = ['CustomerID', 'Genre', 'Age']
-assembler = VectorAssembler(inputCols=feature_cols, outputCol='features')
-data = assembler.transform(customer_data)
+df=pd.read_csv("purchase data.csv")
 
-# Train the K-means clustering model
-kmeans = KMeans(k=5, seed=42)
-model = kmeans.fit(data)
 
-# Make predictions
-predictions = model.transform(data)
-
-# Evaluate the clustering results using WCSS
-evaluator = ClusteringEvaluator()
-wcss = evaluator.evaluate(predictions)
-print("Within-Cluster Sum of Squares (WCSS):", wcss)
+print(df.describe())
+print(df.info())
