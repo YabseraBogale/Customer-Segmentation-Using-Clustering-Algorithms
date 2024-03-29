e45c606589af61f3bd63af012b64b658c8fdb2b1
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-03-29 19:15:20 +0300

    customer segmentation.py have errors in it trying to fix it

--- customer segmentation.py
+++ customer segmentation.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-df = pd.read_csv('Mall_Customer.csv')
+df = pd.read_csv('Mall_Customers.csv')
 df.head(10)
 df.shape
 df.info()
-x = df.iloc[:, [3,4]].values
+X = df.iloc[:, [3,4]].values
 from sklearn.cluster import KMeans
 wcss = []
-for i in range(1,11);
+for i in range(1,11):
     kmeans = KMeans(n_clusters= i, init='k-means++', random_state=0)
     kmeans.fit(X)
     wcss.append(kmeans.inertia_)
@@ -28,7 +28,7 @@ plt.scatter(X[y_kmeans == 1,0], X[y_kmeans == 1,1], s= 80, c= "blue", label='cus
 plt.scatter(X[y_kmeans == 2,0], X[y_kmeans == 2,1], s= 80, c= "yellow", label='customer 3')
 plt.scatter(X[y_kmeans == 3,0], X[y_kmeans == 3,1], s= 80, c= "cyan", label='customer 4')
 plt.scatter(X[y_kmeans == 4,0], X[y_kmeans == 4,1], s= 80, c= "black", label='customer 5')
-plt,scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=100, c= 'magenta' label= 'centroid')
+plt,scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=100, c= 'magenta' ,label= 'centroid')
 plt.title('clusters of customers')
 plt.xlabel('annual income (k$)')
 plt.ylabel('spending score (1-100)')
