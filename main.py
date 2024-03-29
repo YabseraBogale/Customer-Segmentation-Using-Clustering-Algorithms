51d9a3c518ac93b0f534fd06100da5227feef6d4
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-03-29 19:04:09 +0300

    ok

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 df=pd.read_csv("Mall_Customers.csv")
 
 
-print(df.describe())
-print(df.info())
+
+print(df.isnull().sum())
