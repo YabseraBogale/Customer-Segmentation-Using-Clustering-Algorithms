838e69f8c05560f15e3534715c06ba6ae7284dad
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-23 15:32:29 +0300

    ok

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
 
 df=pd.read_csv("Mall_Customers.csv")
 
 
+sns.scatterplot(data=df,x='Age',y='Spending_Score',hue = 'Annual_Income_(k$)')
 
-print(df.isnull().sum())
