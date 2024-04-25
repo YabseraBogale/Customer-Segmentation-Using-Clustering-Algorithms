ce24cacd66c4a444a61407fb71c302dad0c1fbda
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-25 19:32:20 +0300

    ok

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-
-df=pd.read_csv("Mall_Customers.csv")
-
-
-sns.scatterplot(data=df,x='Age',y='Spending_Score',hue = 'Annual_Income_(k$)')
-
+from sklearn import preprocessing
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
