1bf5c08c1b9e4664ad5631a5ca611b300720c632
lulitfiseha <lulitfiseha@gmail.com>
2024-05-16 01:19:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
+
+home_data = pd.read_csv('housing.csv', usecols = ['longitude', 'latitude', 'median_house_value'])
+home_data.head()
+sns.scatterplot(data = home_data, x = 'longitude', y = 'latitude', hue = 'median_house_value')
+X_train, X_test, y_train, y_test = train_test_split(home_data[['latitude', 'longitude']], home_data[['median_house_value']], test_size=0.33, random_state=0)
+X_train_norm = preprocessing.normalize(X_train)
+X_test_norm = preprocessing.normalize(X_test)
+kmeans = KMeans(n_clusters = 3, random_state = 0, n_init='auto')
+kmeans.fit(X_train_norm)
+sns.scatterplot(data = X_train, x = 'longitude', y = 'latitude', hue = kmeans.labels_)
+sns.boxplot(x = kmeans.labels_, y = y_train['median_house_value'])
+silhouette_score(X_train_norm, kmeans.labels_, metric='euclidean')
+K = range(2, 8)
+fits = []
+score = []
