35bac8eda10067b59fb35b1498b03ca469d6a547
TAREKEGN TADELE <118010414+Tarekegn10@users.noreply.github.com>
2024-05-30 21:55:47 +0300

    Update Clustering Algorithms.py

--- Clustering Algorithms.py
+++ Clustering Algorithms.py
@@ -84,20 +84,3 @@ plt.ylabel('Annual spend from customer on site')
 plt.legend()
 plt.show()
 
-
-
-km4=KMeans(n_clusters=6,init='k-means++', max_iter=300, n_init=10, random_state=0)
-y_means = km4.fit_predict(X)
-#Visualizing the clusters
-plt.scatter(X[y_means==0,0],X[y_means==0,1],s=50, c='purple',label='Cluster1')
-plt.scatter(X[y_means==1,0],X[y_means==1,1],s=50, c='blue',label='Cluster2')
-plt.scatter(X[y_means==2,0],X[y_means==2,1],s=50, c='green',label='Cluster3')
-plt.scatter(X[y_means==3,0],X[y_means==3,1],s=50, c='cyan',label='Cluster4')
-plt.scatter(X[y_means==4,0],X[y_means==4,1],s=50, c='magenta',label='Cluster5')
-plt.scatter(X[y_means==5,0],X[y_means==5,1],s=50, c='orange',label='Cluster6')
-plt.scatter(km.cluster_centers_[:,0], km.cluster_centers_[:,1],s=200,marker='s', c='red', alpha=0.7, label='Centroids')
-plt.title('Customer segments')
-plt.xlabel('Annual income of customer')
-plt.ylabel('Annual spend from customer on site')
-plt.legend()
-plt.show()
